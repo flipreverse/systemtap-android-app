588740bdbe44820101c5e43eed9ebfe88c15cc3c
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2012-09-23 19:25:39 +0200

    initial commit

---
+++ src/edu/udo/cs/ess/systemtap/SystemtapActivity.java
@@ -0,0 +1,35 @@
+package edu.udo.cs.ess.systemtap;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class SystemtapActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_systemtap);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_systemtap, menu);
+        return true;
+    }
+
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
